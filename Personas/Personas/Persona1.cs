622c6a0a449ec41669c61a0fd375b43e3aa3032a
josemoreta <josemoreta27@gmail.com>
2018-11-08 12:41:03 -0800

    classe 3

---
+++ Personas/Personas/Persona1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    class Persona1
+    {
+        public string nombre;
+        public string apellidos;
+        public string direccion;
+        protected string ciudad;
+        internal int edad;
+
+        public Persona1()
+        {
+            nombre = "Jose Moreta";
+            apellidos = "Moreta Jaquez";
+            direccion = "12 de julio";
+            ciudad = "puerto plata";
+            edad = 25;
+        }
+        
+        public Persona1 (string Nombre,String Apellido,Int16 Edad)
+        {
+            nombre = Nombre;
+            apellidos = Apellido;
+            edad =Edad;
+            
+        }
+
+        //encasuplacion 
+        public string CiudadPerdida()
+        {
+            return ciudad;
+        }
+
+
+    }
+}
---
+++ Personas/Personas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          /*  Persona1 persona1 = new Persona1();
+
+            Console.WriteLine(persona1.nombre);
+            Console.WriteLine(persona1.CiudadPerdida()); 
+
+            Console.ReadKey(); */
+
+
+
+            bool boo = false;
+
+            for (int x = 0; x <= 10; x++)
+            {
+                if (boo)
+                {
+                    Console.WriteLine("El valor de boo es TRUE");
+                    
+                }
+
+                else
+                {
+                    Console.WriteLine("El Valor de boo es False");
+                        
+                }
+
+                if (boo)
+                    boo = false;
+                else boo = true;
+
+
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+
+
+
+    }
+}
